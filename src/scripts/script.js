534cd3a8646320a5739155760bdedc5ef5b419d7
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2022-02-04 01:10:25 -0300

    voltando alterações

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -41,7 +41,7 @@ gltfLoader.load("/models/characterFinal.gltf", (gltf) => {
   const idle = mixer.clipAction(gltf.animations[0]);
   idle.play();
   scene.add(character);
-  // updateMaterials(character);
+  updateMaterials(character);
 });
 
 /**
@@ -51,23 +51,23 @@ gltfLoader.load("/models/characterFinal.gltf", (gltf) => {
 const ambientLight = new THREE.AmbientLight(0x404040, 2);
 scene.add(ambientLight);
 
-// const light = new THREE.PointLight(0xffffff, 6, 50);
-// light.position.set(1, 1, 1);
-// light.castShadow = false;
-// light.shadow.bias = -0.002;
-// light.shadow.camera.near = 1;
-// light.shadow.camera.far = 1;
-// light.shadow.camera.fov = 90;
-// light.shadow.mapSize.width = 2048;
-// light.shadow.mapSize.height = 2048;
-// light.shadow.radius = 8;
-// scene.add(light);
-// // const sphereSize = 1;
-// const pointLightHelper = new THREE.PointLightHelper(light, sphereSize);
-// scene.add(pointLightHelper);
+const light = new THREE.PointLight(0xffffff, 6, 50);
+light.position.set(1, 1, 1);
+light.castShadow = true;
+light.shadow.bias = -0.002;
+light.shadow.camera.near = 1;
+light.shadow.camera.far = 1;
+light.shadow.camera.fov = 90;
+light.shadow.mapSize.width = 2048;
+light.shadow.mapSize.height = 2048;
+light.shadow.radius = 8;
+scene.add(light);
+const sphereSize = 1;
+const pointLightHelper = new THREE.PointLightHelper(light, sphereSize);
+scene.add(pointLightHelper);
 
-// const directionalLight = new THREE.DirectionalLight( 0xffffff, 3 );
-// scene.add( directionalLight );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 3 );
+scene.add( directionalLight );
 
 /**
  * Click Interation
@@ -115,35 +115,35 @@ const updateMaterials = (scene) => {
  * Objects
  */
 
-// const sphereGeometry = new THREE.SphereBufferGeometry(1, 2, 2);
-// const boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
+const sphereGeometry = new THREE.SphereBufferGeometry(1, 2, 2);
+const boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
 
-// const geometriesArray = [sphereGeometry, boxGeometry];
+const geometriesArray = [sphereGeometry, boxGeometry];
 
-// const boxMaterial = new THREE.MeshBasicMaterial({
-//   color: "#e57373",
-//   wireframe: true,
-//   opacity: 0.1,
-//   transparent: true,
-// });
-// let boxMeshArray = new Array(80);
+const boxMaterial = new THREE.MeshBasicMaterial({
+  color: "#e57373",
+  wireframe: true,
+  opacity: 0.1,
+  transparent: true,
+});
+let boxMeshArray = new Array(80);
 
-// const geometryGroup = new THREE.Group();
+const geometryGroup = new THREE.Group();
 
-// for (let i = 0; i < boxMeshArray.length; i++) {
-//   const geometry =
-//     geometriesArray[Math.floor(Math.random() * geometriesArray.length)];
-//   boxMeshArray[i] = new THREE.Mesh(geometry, boxMaterial);
+for (let i = 0; i < boxMeshArray.length; i++) {
+  const geometry =
+    geometriesArray[Math.floor(Math.random() * geometriesArray.length)];
+  boxMeshArray[i] = new THREE.Mesh(geometry, boxMaterial);
 
-//   boxMeshArray[i].position.x = (Math.random() - 0.5) * 3;
-//   boxMeshArray[i].position.y = (Math.random() - 0.5) * 3;
-//   boxMeshArray[i].position.z = Math.min((Math.random() - 0.5) * 3, -2);
-//   boxMeshArray[i].scale.set(0.2, 0.2, 0.2);
+  boxMeshArray[i].position.x = (Math.random() - 0.5) * 3;
+  boxMeshArray[i].position.y = (Math.random() - 0.5) * 3;
+  boxMeshArray[i].position.z = Math.min((Math.random() - 0.5) * 3, -2);
+  boxMeshArray[i].scale.set(0.2, 0.2, 0.2);
 
-//   geometryGroup.add(boxMeshArray[i]);
-// }
+  geometryGroup.add(boxMeshArray[i]);
+}
 
-// scene.add(geometryGroup);
+scene.add(geometryGroup);
 
 /**
  * Sizes
@@ -191,7 +191,7 @@ const onMouseMove = (event) => {
 };
 const onMouseClick = () => {
   if (character) {
-    // updateMaterials(character);
+    updateMaterials(character);
   }
 };
 
@@ -230,9 +230,9 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.setClearColor("#0e1212");
 renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
-// renderer.shadowMap.enabled = true;
-// renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-// renderer.physicallyCorrectLights = true;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+renderer.physicallyCorrectLights = true;
 
 /**
  * Animate
@@ -247,12 +247,12 @@ const tick = () => {
   const deltaTime = elapsedTime - lastElapsedTime;
   lastElapsedTime = elapsedTime;
 
-  // for (let i = 0; i < boxMeshArray.length; i++) {
-  //   boxMeshArray[i].rotateY(Math.cos(elapsedTime) * 0.00005 * i);
-  //   boxMeshArray[i].rotateZ(Math.cos(elapsedTime) * 0.00005 * i);
-  //   boxMeshArray[i].rotateX(Math.cos(elapsedTime) * 0.00005 * i);
-  //   boxMeshArray[i].position.y += Math.sin(elapsedTime + i) * 0.0005;
-  // }
+  for (let i = 0; i < boxMeshArray.length; i++) {
+    boxMeshArray[i].rotateY(Math.cos(elapsedTime) * 0.00005 * i);
+    boxMeshArray[i].rotateZ(Math.cos(elapsedTime) * 0.00005 * i);
+    boxMeshArray[i].rotateX(Math.cos(elapsedTime) * 0.00005 * i);
+    boxMeshArray[i].position.y += Math.sin(elapsedTime + i) * 0.0005;
+  }
 
   if (mixer) {
     mixer.update(deltaTime);
